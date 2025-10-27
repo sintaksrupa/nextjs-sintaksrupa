b8356e7b075ba722857bc140f149b17be90fc374
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-10-27 22:55:36 +0700

    chore(seo): update metadata titles and keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ const fontSans = Urbanist({
 export const metadata: Metadata = {
   title: {
     template: "%s | SintaksRupa",
-    default: "Jasa Desain Grafis & Website Terjangkau Terpercaya | SintaksRupa",
+    default: "SintaksRupa - Jasa Desain Grafis & Website Terjangkau Terpercaya",
   },
   description:
     "Sintaks Rupa adalah partner kreatif digital yang berbasis di Klaten, Jawa Tengah. Kami adalah spesialis dalam layanan desain grafis dan pengembangan website. Semangat kami adalah membantu bisnis dan UMKM untuk bertumbuh dan tampil profesional di dunia digital melalui identitas visual yang kuat dan website yang optimal.",
@@ -31,24 +31,34 @@ export const metadata: Metadata = {
     "Sintaks Rupa",
     "Jasa Desain Grafis",
     "Jasa Website",
+    "Jasa Desain Grafis dan Website",
     "Jasa Pembuatan Desain Grafis",
     "Jasa Pengembangan Website",
+    "Jasa Pembuatan Desain Grafis dan Website",
     "Jasa Desain Grafis Terpercaya",
     "Jasa Website Terpercaya",
+    "Jasa Desain Grafis dan Website Terpercaya",
     "Jasa Pembuatan Desain Grafis Terpercaya",
     "Jasa Pengembangan Website Terpercaya",
+    "Jasa Pembuatan Desain Grafis dan Website Terpercaya",
     "Jasa Desain Grafis Murah",
     "Jasa Website Murah",
+    "Jasa Desain Grafis dan Website Murah",
     "Jasa Pembuatan Desain Grafis Murah",
     "Jasa Pengembangan Website Murah",
+    "Jasa Pembuatan Desain Grafis dan Website Murah",
     "Jasa Desain Grafis Klaten",
     "Jasa Website Klaten",
+    "Jasa Desain Grafis dan Website Klaten",
     "Jasa Pembuatan Desain Grafis Klaten",
     "Jasa Pengembangan Website Klaten",
+    "Jasa Pembuatan Desain Grafis dan Website Klaten",
     "Jasa Desain Grafis Jawa Tengah",
     "Jasa Website Jawa Tengah",
+    "Jasa Desain Grafis dan Website Jawa Tengah",
     "Jasa Pembuatan Desain Grafis Jawa Tengah",
     "Jasa Pengembangan Website Jawa Tengah",
+    "Jasa Pembuatan Desain Grafis dan Website Jawa Tengah",
   ],
   authors: [
     {
@@ -61,12 +71,11 @@ export const metadata: Metadata = {
     telephone: false,
   },
   openGraph: {
-    title: "Jasa Desain Grafis & Website Terjangkau Terpercaya | SintaksRupa",
+    title: "SintaksRupa - Jasa Desain Grafis & Website Terjangkau Terpercaya",
     description:
       "Sintaks Rupa adalah partner kreatif digital yang berbasis di Klaten, Jawa Tengah. Kami adalah spesialis dalam layanan desain grafis dan pengembangan website. Semangat kami adalah membantu bisnis dan UMKM untuk bertumbuh dan tampil profesional di dunia digital melalui identitas visual yang kuat dan website yang optimal.",
     url: "https://sintaksrupa.site/",
-    siteName:
-      "SintaksRupa | Jasa Desain Grafis & Website Terjangkau Terpercaya",
+    siteName: "SintaksRupa",
     images: [
       {
         url: "https://qbbs2frdex6nzjnr.public.blob.vercel-storage.com/primary-logo-type-sintaks-rupa.jpg",
@@ -86,7 +95,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Jasa Desain Grafis & Website Terjangkau Terpercaya | SintaksRupa",
+    title: "SintaksRupa - Jasa Desain Grafis & Website Terjangkau Terpercaya",
     description:
       "Sintaks Rupa adalah partner kreatif digital yang berbasis di Klaten, Jawa Tengah. Kami adalah spesialis dalam layanan desain grafis dan pengembangan website. Semangat kami adalah membantu bisnis dan UMKM untuk bertumbuh dan tampil profesional di dunia digital melalui identitas visual yang kuat dan website yang optimal.",
     images: [
