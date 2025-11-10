01bfc43a2c3f497de87392536a42cd90b5d3c032
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-11-10 18:09:08 +0700

    style(gallery): adjust layout for gallery section

--- components/galeri/GallerySection.tsx
+++ components/galeri/GallerySection.tsx
@@ -8,10 +8,10 @@ type GallerySectionProps = {
 
 export function GallerySection({ galleries }: GallerySectionProps) {
   return (
-    <div className="flex flex-col gap-36">
+    <div className="flex flex-col items-center justify-center gap-36">
       {galleries.map((gallery) => (
         <section key={gallery.title}>
-          <div className="max-w-7xl grid lg:grid-cols-2 items-center justify-center gap-5 lg:gap-10 xl:gap-20 mx-auto px-3 md:px-5">
+          <div className="max-w-7xl grid lg:grid-cols-2 items-center justify-center gap-5 lg:gap-10 xl:gap-20 mx-3 md:mx-5">
             <GalleryDetails
               title={gallery.title}
               descriptions={gallery.description}
